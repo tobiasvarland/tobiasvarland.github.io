3482624fd1883416b72a97fa2478bf036c30a71d
Toby Varland <toby@tobyvarland.com>
2018-10-10 12:09:41 -0400

    Preload images with javascript instead of HTML

--- js/tv.js
+++ js/tv.js
@@ -3,37 +3,27 @@ var TV = {
   /**
    * Dynamically loads portfolio.
    */
+  portfolioCount: 5,
   loadPortfolio: function() {
-    var folder = 'port/';
-    $.ajax({
-      url: folder,
-      success: function(data) {
-        console.log(data);
-      }
-    });
-  },
-
-  /**
-   * Adds images to portfolio.
-   */
-  buildPortfolio: function() {
 
     // Reference containers.
     var imageContainer = $('#portfolio-photo-container');
     var indicatorContainer = $('#portfolio-indicator-container');
 
     // Add images.
-    $('link[rel="preload"]').each(function(index) {
-      var src = $(this).attr('href');
+    var index = null;
+    var src = null;
+    for (index = 2; index <= TV.portfolioCount; index++) {
+      src = 'port/' + index + '.jpg';
+      $('<img />').attr('src', src);
       imageContainer.append('<div class="carousel-item"><img class="d-block w-100" src="' + src + '"></div>');
-      indicatorContainer.append('<li data-target="#portfolio" data-slide-to="' + (index + 1) + '"></li>');
-    });
+      indicatorContainer.append('<li data-target="#portfolio" data-slide-to="' + (index - 1) + '"></li>');
+    }
 
-  }
+  },
 
 };
 
 $(function() {
-  TV.buildPortfolio();
   TV.loadPortfolio();
 });
