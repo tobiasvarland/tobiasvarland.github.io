4613a7749824a665a84be1de0def6b89809850f3
Toby Varland <toby@tobyvarland.com>
2018-10-10 09:43:54 -0400

    Add image preloading and update color scheme

---
+++ js/tv.js
@@ -0,0 +1,25 @@
+var TV = {
+
+  /**
+   * Adds images to portfolio.
+   */
+  buildPortfolio: function() {
+
+    // Reference containers.
+    var imageContainer = $('#portfolio-photo-container');
+    var indicatorContainer = $('#portfolio-indicator-container');
+
+    // Add images.
+    $('link[rel="preload"]').each(function(index) {
+      var src = $(this).attr('href');
+      imageContainer.append('<div class="carousel-item"><img class="d-block w-100" src="' + src + '"></div>');
+      indicatorContainer.append('<li data-target="#portfolio" data-slide-to="' + (index + 1) + '"></li>');
+    });
+
+  }
+
+};
+
+$(function() {
+  TV.buildPortfolio();
+});
