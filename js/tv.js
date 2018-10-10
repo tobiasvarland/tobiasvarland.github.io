6ff4085305be9116d20fa85b342dd9725400fdd4
Toby Varland <toby@tobyvarland.com>
2018-10-10 15:10:50 -0400

    Change gallery loading to sequential instead of parallel

--- js/tv.js
+++ js/tv.js
@@ -4,26 +4,47 @@ var TV = {
    * Dynamically loads portfolio.
    */
   portfolioCount: 5,
+  imageContainer: null,
+  indicatorContainer: null,
   loadPortfolio: function() {
 
     // Reference containers.
-    var imageContainer = $('#portfolio-photo-container');
-    var indicatorContainer = $('#portfolio-indicator-container');
+    TV.imageContainer = $('#portfolio-photo-container');
+    TV.indicatorContainer = $('#portfolio-indicator-container');
 
-    // Add images.
-    var index = null;
-    var src = null;
-    for (index = 2; index <= TV.portfolioCount; index++) {
-      src = 'port/' + index + '.jpg';
-      $('<img />').attr('src', src);
-      imageContainer.append('<div class="carousel-item"><img class="d-block w-100" src="' + src + '"></div>');
-      indicatorContainer.append('<li data-target="#portfolio" data-slide-to="' + (index - 1) + '"></li>');
-    }
+    // Load first image.
+    TV.loadImage(1);
+
+  },
+
+  /**
+   * Loads image and calls for next image to be loaded.
+   */
+  loadImage: function(index) {
+
+    // Add image to gallery, and load next image when finished.
+    var path = 'port/' + index + '.jpg';
+    var img = $('<img />').addClass('d-block w-100');
+    img.on('load', function() {
+      var galleryImage = $('<div />').addClass('carousel-item').append(img);
+      var indicator = $('<li />').attr('data-target', '#portfolio').attr('data-slide-to', index - 1);
+      if (index == 1) {
+        galleryImage.addClass('active');
+        indicator.addClass('active');
+      }
+      TV.imageContainer.append(galleryImage);
+      TV.indicatorContainer.append(indicator);
+      if (index < TV.portfolioCount) {
+        TV.loadImage(index + 1);
+      }
+    });
+    img.attr('src', path);
 
   },
 
 };
 
-$(function() {
+$(window).on('load', function() {
   TV.loadPortfolio();
 });
+// $(function() { console.log('1'); });
